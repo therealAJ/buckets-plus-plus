46d3b39f98c93c677b2936ac8bb71a13c6928d31
Jason Wong <j.wong.9@alumni.ubc.ca>
2016-10-15 10:55:09 -0700

    retrieve form data

--- server/logistic_regression.py
+++ server/logistic_regression.py
@@ -1,2 +1,3 @@
-def helloworld():
-	return "Hello World"
+def predict(combined_shot_type, loc_x, loc_y, seconds_remaining, period, season, shot_made, shot_type, game_date, opponent):
+	print combined_shot_type
+	return combined_shot_type
--- server/server.py
+++ server/server.py
@@ -1,12 +1,21 @@
 from flask import Flask, request
-from logistic_regression import helloworld
+from logistic_regression import predict
 
 app = Flask(__name__)
 
 @app.route('/', methods=["POST"])
-def hello_world():
-    #return helloworld()
-    return request.form["hello"]
+def startPrediction():
+    combined_shot_type = request.form["combined_shot_type"]
+    loc_x = request.form["loc_x"]
+    loc_y = request.form["loc_y"]
+    seconds_remaining = request.form["seconds_remaining"]
+    period = request.form["period"]
+    season = request.form["season"]
+    shot_made = request.form["shot_made"]
+    shot_type_flag = request.form["shot_type_flag"]
+    game_date = request.form["game_date"]
+    opponent = request.form["opponent"]
+    return predict(combined_shot_type, loc_x, loc_y, seconds_remaining, period, season, shot_made, shot_type_flag, game_date, opponent)
 
 if __name__ == '__main__':
     app.run(debug=True)
