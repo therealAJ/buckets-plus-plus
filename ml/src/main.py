0198ee96f0c0561290e53c520b8af47c622cc4e7
Decepticon <autranemorais@gmail.com>
2016-10-15 10:13:09 -0700

    testing pandas

--- ml/src/main.py
+++ ml/src/main.py
@@ -1,3 +1,17 @@
 import pandas as pd
 
-data = pd.read_csv("../")
+data = pd.read_csv("../../csv/data.csv")
+
+data.set_index('shot_id', inplace=True)
+data["action_type"] = data["action_type"].astype('object')
+data["combined_shot_type"] = data["combined_shot_type"].astype('category')
+data["game_event_id"] = data["game_event_id"].astype('category')
+data["game_id"] = data["game_id"].astype('category')
+data["period"] = data["period"].astype('object')
+data["playoffs"] = data["playoffs"].astype('category')
+data["season"] = data["season"].astype('category')
+data["shot_made_flag"] = data["shot_made_flag"].astype('category')
+data["shot_type"] = data["shot_type"].astype('category')
+data["team_id"] = data["team_id"].astype('category')
+
+print(data["team_id"])
