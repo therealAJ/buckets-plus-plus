d6580a4d18f3dc1303d0a231d54b6adf54deaca7
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2016-10-16 07:55:32 -0700

    Create createHeapMap

--- ui/static/client.js
+++ ui/static/client.js
@@ -72,3 +72,47 @@ function onMouseDrag(event) {
 function onMouseUp(event) {
 
 }
+
+var rec_map = [];
+
+var drawGridRects = function(num_rectangles_wide, num_rectangles_tall, boundingRect) {
+  var width_per_rectangle = boundingRect.width / num_rectangles_wide;
+  var height_per_rectangle = boundingRect.height / num_rectangles_tall;
+  for (var i = 0; i < num_rectangles_wide; i++) {
+    for (var j = 0; j < num_rectangles_tall; j++) {
+      var aRect = new paper.Path.Rectangle(boundingRect.left + i * width_per_rectangle, boundingRect.top + j * height_per_rectangle, width_per_rectangle, height_per_rectangle);
+      aRect.strokeColor = 'white';
+			rec_map.push(aRect);
+    }
+  }
+}
+
+function createHeatString(rec_map) {
+	var output = {};
+
+	// Put Time in seconds
+	output['time'] = "111" // FIX THIS
+
+	// x string
+	var output_x = "";
+	for (var k = 0; k < rec_map.length; k++) {
+		output_x += rec_map[k].position._x.toFixed(3).toString() + ","
+	}
+	output_x.substring(0, output_x.length - 1);
+	output['output_x'] = output_x;
+
+	var output_y = "";
+	for (var r = 0; r < rec_map.length; r++) {
+		output_y += rec_map[r].position._y.toFixed(3).toString() + ","
+	}
+	output_y.substring(0, output_y.length - 1);
+	output['output_y'] = output_y;
+
+	return output;
+}
+
+drawGridRects(30, 30, paper.view.bounds);
+
+console.log(rec_map[11].position._x);
+
+console.log(createHeatString(rec_map));
