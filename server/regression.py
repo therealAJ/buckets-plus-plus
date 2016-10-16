0db3b0aed4a1e5d49689be9c35fa813a08db02dd
Alex <alexonej@gmail.com>
2016-10-15 20:35:38 -0700

    redo folder struc

---
+++ server/regression.py
@@ -0,0 +1,23 @@
+import pandas
+from sklearn import cross_validation
+from sklearn.linear_model import LogisticRegression
+import pickle
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = pandas.read_csv(url, names=names)
+array = dataframe.values
+X = array[:,0:8]
+Y = array[:,8]
+test_size = 0.33
+seed = 7
+X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(X, Y, test_size=test_size, random_state=seed)
+# Fit the model on 33%
+model = LogisticRegression()
+model.fit(X_train, Y_train)
+# save the model to disk
+filename = 'finalized_model.sav'
+pickle.dump(model, open(filename, 'wb'))
+
+loaded_model = pickle.load(open(filename, 'rb'))
+result = loaded_model.score(X_test, Y_test)
+print(result)
