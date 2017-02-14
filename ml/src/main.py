3be9e2478669551aa39e501f500768e974606a8a
Alex <alexonej@gmail.com>
2017-02-13 20:21:03 -0800

    clean up ml a bit

--- ml/src/main.py
+++ ml/src/main.py
@@ -6,6 +6,7 @@ from collections import OrderedDict
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.feature_selection import VarianceThreshold, RFE, SelectKBest, chi2
 from sklearn.cross_validation import KFold, cross_val_score
+from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.grid_search import GridSearchCV
 
 from pandas import DataFrame
@@ -45,33 +46,18 @@ data = raw_data.dropna()
 data["time_remaining"] = data["minutes_remaining"] * 60 + data["seconds_remaining"]
 data.drop("minutes_remaining", axis=1, inplace=True)
 data.drop("seconds_remaining", axis=1, inplace=True)
-
-
-#is this shot clutch af?
-#data["clutch_shot"] = data["time_remaining"] < 5
-#data.drop("time_remaining", axis=1, inplace=True)
-
-
-#is this game at home
-#data['home_game'] = data['matchup'].str.contains('vs').astype('int')
 data.drop("matchup", axis=1, inplace=True)
 
-#game date feature?? TBD
-
-
-#convert to polar
+#convert to polar coordinates to increase accuracy
 zero = data['loc_x'] == 0
 data['angle'] = np.arctan(data['loc_y'][~zero] / data['loc_x'][~zero])
 data['angle'][zero] = np.pi / 2
 
 data['distance'] = np.sqrt(data['loc_y'] ** 2 + data['loc_x'] ** 2)
 
-
-#data['loc_x'] = pd.cut(data['loc_x'], 25)
-#data['loc_y'] = pd.cut(data['loc_y'], 25)
+#remove redundant location columns now that we have a distance column
 data.drop('loc_x', axis=1, inplace=True)
 data.drop('loc_y', axis=1, inplace=True)
-
 
 #convert categorical data to one-hot encoding
 categories = []
@@ -89,7 +75,6 @@ data.drop("shot_made_flag", axis=1, inplace=True) #remove labels from data
 X = data
 Y = target
 
-
 kfold = KFold(n=len(X), n_folds=3, random_state=7)
 scoring = 'log_loss'
 
@@ -102,13 +87,16 @@ model = GradientBoostingClassifier(n_estimators=100, random_state=7)
 results = cross_val_score(model, X, Y, cv=kfold, scoring=scoring, n_jobs=1)
 print("({0:.3f}) +/- ({1:.3f})".format(results.mean(), results.std()))
 
-
-
+#exploratory data analysis
 print(Y.shape)
+
+#fit GBC with input and target data (should really have used test train split but `\_(")_/`
 model.fit(X, Y)
+
+predictions = model.predict(X)
+
 print(model.predict_proba(df))
-
-
+print (classification_report(Y, predictions))
+#average precision of 0.62 ----> could have been better if trained with more 
 
 print("done")
-
